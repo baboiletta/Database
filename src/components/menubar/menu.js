c836666e7bbd20137da0c111c8c0d211e63af7e6
Dai <baboiletta@gmail.com>
2019-09-23 21:02:38 +0700

    dangonngu

--- src/components/menubar/menu.js
+++ src/components/menubar/menu.js
@@ -1,44 +1,65 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import { FaUser } from "react-icons/fa";
+import { locale } from '../../config/local'
 
 class MenuBar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lang: 'vi'
+    }
+    console.log(locale)
+    locale.setLanguage(this.state.lang)
+  }
+  switchLanguage = () => {
+    this.setState({
+      lang: this.state.lang == 'vi' ? 'en' : 'vi'
+    })
+    locale.setLanguage(this.state.lang)
+    this.setState({})
+  }
   render() {
     return (
 
       <React.Fragment>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">Green Academy</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/profile">Ptofile</Nav.Link>
-              {/* <Nav.Link href="/khoadaotao">Khoa Dao Tao</Nav.Link> */}
-              <NavDropdown title="Khoa Dao Tao" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Tieng Han</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Web Design</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Mobile</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">FullStack Developer</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/blog">Blog</Nav.Link>
-              <Nav.Link href="/gioithieu">Gioi Thieu</Nav.Link>
-              <Nav.Link href="/tuyendung">Tuyen Dung</Nav.Link>
-              <Nav.Link href="/lienhe">Lien He</Nav.Link>
+        <div className="container-fluid">
 
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-              <FaUser />
-              <NavDropdown title="   " id="basic-nav-dropdown">
-                <NavDropdown.Item href="/logout">Dang xuat</NavDropdown.Item>
-            
-              </NavDropdown>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
 
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">Green Academy</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/profile">{locale.training}</Nav.Link>
+                {/* <Nav.Link href="/khoadaotao">Khoa Dao Tao</Nav.Link> */}
+                <NavDropdown title="Khoa Dao Tao" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Tieng Han</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Web Design</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Mobile</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">FullStack Developer</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/blog">{locale.blog}</Nav.Link>
+                <Nav.Link href="/gioithieu">{locale.about}</Nav.Link>
+                <Nav.Link href="/tuyendung">{locale.hiring}</Nav.Link>
+                <Nav.Link href="/lienhe">{locale.contact}</Nav.Link>
+
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success" className="mr-2">{locale.search}</Button>
+                <FaUser />
+                <NavDropdown title="   " id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/logout">{locale.logout}</NavDropdown.Item>
+                </NavDropdown>
+                <Button className="primary" onClick={() => this.switchLanguage()}> {locale.lang}</Button>
+                {/* <Button className="primary" onClick={() => this.switchLanguage()}> vi</Button> */}
+
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
 
       </React.Fragment>
 
---
+++ src/config/local.js
@@ -0,0 +1,36 @@
+import LocalizedString from 'react-localization'
+
+export const locale = new LocalizedString(
+    {
+        vi: {
+            lang: 'vi',
+            home: 'Trang Chủ',
+            contact: 'Liên Hệ',
+            about: 'Giới Thiệu',
+            training: 'Khóa Đào Tạo',
+            login: 'Đăng nhập',
+            logout: 'Đăng xuất',
+            blog: 'Blog',
+            hiring: 'Tuyển Dụng',
+            phone: 'Số Điện Thoại',
+            password: 'Mật Khẩu',
+            search: 'Tìm Kiếm'
+
+        }
+        ,
+        en: {
+            lang: 'en',
+            home: 'Home',
+            contact: 'Contact',
+            about: 'About Us',
+            training: 'Training',
+            login: 'Login',
+            logout: 'Logout',
+            blog: 'Blog',
+            hiring: 'Hiring',
+            phone: 'Phone Number',
+            password: 'Password',
+            search: 'Search'
+        }
+    }
+)
