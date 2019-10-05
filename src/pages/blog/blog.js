353d500af18b5ab844303e6a372c5fc5db6735a1
TT <baboieltta@gmail.com>
2019-10-05 11:41:31 +0700

    tạo blog

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,14 +1,125 @@
 import React from 'react';
 import FooterPage from '../../components/footer/footer';
+import MenuBar from '../../components/menubar/menu';
+import Tincon from '../../components/blogs/tincon';
+import b1 from './../../assets/anh/img/b1.jpg';
+import b2 from './../../assets/anh/img/b2.jpeg';
+import b3 from './../../assets/anh/img/b3.jpeg';
+import b4 from './../../assets/anh/img/b4.jpg';
+import b5 from './../../assets/anh/img/b5.jpg';
+import b6 from './../../assets/anh/img/b6.jpg';
+import b7 from './../../assets/anh/img/b7.jpg';
 
-class Blog extends React.Component{
-    render(){
-        return(
+class Blog extends React.Component {
+    render() {
+        return (
             <React.Fragment>
-                 <div>Blog</div>
-            <FooterPage></FooterPage>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <Tincon ba={b1}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon><hr/>
+                                <Tincon ba={b2}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon><hr/>
+                            <Tincon ba={b3}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon><hr/>
+                            <Tincon ba={b4}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon><hr/>
+                     
+                            <Tincon ba={b5}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon><hr/>
+                            <Tincon ba={b6}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon><hr/>
+                            <Tincon ba={b7}
+                                title="A Guide For Entrepreneurs to Achieve a Work-Life Balance"
+                                text="Being an entrepreneur is no easy task. The long hours of work can be
+                                frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right  frustrating and can sometimes 
+                             lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the rightThere are indeed several ways on
+                              how you can keep the right  frustrating and can sometimes lead to burnout.Is it even possible to maintain a
+                             work-life balance amidst the most stringent of working hours? The answer is a definite Yes!
+                             There are indeed several ways on how you can keep the right"
+                                ></Tincon>
+                            
+                </div>
+                </div>
+                </div>
+            
+            {/* <FooterPage></FooterPage> */}
+
+            
         
-            </React.Fragment>
+            </React.Fragment >
         )
     }
 }
