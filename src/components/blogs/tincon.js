b3ffbb7e9c4f4cdee997f2d2941bab686fb6945f
TT <baboieltta@gmail.com>
2019-10-09 16:13:46 +0700

    fixed ne

--- src/components/blogs/tincon.js
+++ src/components/blogs/tincon.js
@@ -11,40 +11,40 @@ class Tincon extends Component {
 
     chuyenDoiURL = (str) => {
 
-
-        // Chuyển hết sang chữ thường
-        str = str.toLowerCase();
-
-        // xóa dấu
-        str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
-        str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
-        str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
-        str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
-        str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
-        str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
-        str = str.replace(/(đ)/g, 'd');
-
-        // Xóa ký tự đặc biệt
-        str = str.replace(/([^0-9a-z-\s])/g, '');
-
-        // Xóa khoảng trắng thay bằng ký tự -
-        str = str.replace(/(\s+)/g, '-');
-
-        // xóa phần dự - ở đầu
-        str = str.replace(/^-+/g, '');
-
-        // xóa phần dư - ở cuối
-        str = str.replace(/-+$/g, '');
-    }
-
-
+            // Chuyển hết sang chữ thường
+            str = str.toLowerCase();     
+         
+            // xóa dấu
+            str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+            str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+            str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
+            str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+            str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+            str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+            str = str.replace(/(đ)/g, 'd');
+         
+            // Xóa ký tự đặc biệt
+            str = str.replace(/([^0-9a-z-\s])/g, '');
+         
+            // Xóa khoảng trắng thay bằng ký tự -
+            str = str.replace(/(\s+)/g, '-');
+         
+            // xóa phần dự - ở đầu
+            str = str.replace(/^-+/g, '');
+         
+            // xóa phần dư - ở cuối
+            str = str.replace(/-+$/g, '');
+         
+            // return
+            return str;
+        }
     render() {
         return (
             <div className="mottin">
 
-                <Link to={"/blog/" + this.chuyenDoiURL(this.props.title)+ "-" + this.props.id + ".html"}>
-                    <h5 className=" tieude">{this.props.title}</h5>
-                    <img className="anhtieude mt-3" src={this.props.ba} alt="Card image cap" /> </Link>
+                <Link to={"/blog/" + this.chuyenDoiURL(this.props.title)+ "." + this.props.id + ".html"}>
+                    <h5 className=" tieude mt-3">{this.props.title}</h5>
+                    <img className="anhtieude mt-3 pt-3" src={this.props.ba} alt="Card image cap" /> </Link>
                     <div className="ochua">
                         <p className="text">{this.props.text}</p>
                     </div>
---
+++ src/components/tinlienquan/relate.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import dulieu from './../dulieu/dulieu.json'
+import Recent from './../recentPost/recent'
+
+class Relate extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Relate;
